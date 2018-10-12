ab209e81772da85608a6a0014ea437c55164c364
Daquaris Chadwick <sourhead5@gmail.com>
2018-10-12 00:08:11 -0400

    created initial login page with basic login functionality

--- outfitter-mobile-app/App.js
+++ outfitter-mobile-app/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ToastAndroid } from 'react-native';
 
 export default class App extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.login = this.login.bind(this);
+    this.state = {user: '', pass: ''};
+  }
+
+  login(){
+    ToastAndroid.show(`u: ${this.state.user} p: ${this.state.pass}`, ToastAndroid.SHORT);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TextInput 
+          style={{height:48, width:200, fontSize: 24}} 
+          placeholder="username" 
+          onChangeText={(user) => {this.setState({user})}}
+          />
+        <TextInput 
+          style={{height:48, width:200, fontSize: 24}} 
+          placeholder="password" 
+          textContentType="password"
+          secureTextEntry={true}
+          onChangeText={(pass) => {this.setState({pass})}}
+          />
+        <Button 
+          onPress={this.login} 
+          title="Login"
+          color="#841584"
+          accessibilityLabel="Login"
+          />
       </View>
     );
   }
